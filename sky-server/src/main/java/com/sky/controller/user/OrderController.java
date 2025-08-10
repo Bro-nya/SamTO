987b450807e4c44812a456c78f44beb4456a33a0
Bro-nya <1572205083@qq.com>
2025-08-10 19:31:15 +0800

    来单提醒业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,9 +1,11 @@
 package com.sky.controller.user;
 
+import com.alibaba.fastjson.JSON;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.websocket.WebSocketServer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user/order")
 @Api(tags = "用户端订单相关接口")
@@ -21,6 +26,10 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    //TODO
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     /**
      * 用户下单
@@ -33,6 +42,15 @@ public class OrderController {
     public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
         log.info("用户下单参数为:{}", ordersSubmitDTO);
         OrderSubmitVO orderSubmitVO=orderService.submitOrder(ordersSubmitDTO);
+        //TODO
+        //通过websocket向客户端浏览器推送消息
+        Map map=new HashMap<>();
+        map.put("type",1);//1来单提醒2客户催单
+        map.put("orderId",orderSubmitVO.getId());
+        map.put("content", "订单号："+orderSubmitVO.getOrderNumber());
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
         return Result.success(orderSubmitVO);
     }
 }
